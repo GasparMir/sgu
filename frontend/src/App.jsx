679c040ea24decddc18bc0528e5bb32908f27874
GasparMir <20193tn015@utez.edu.mx>
2025-10-23 11:33:46 -0600

    [FIX] File import fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import UsersPage from './pages/UsersPage';
+import UsersPage from './pages/UserPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <UsersPage />
+      <UserPage />
     </div>
   );
 }
